62f1c17de8c77e7ba1e080e6ec2510249b051f5e
MalKhar <brunocbranco16@gmail.com>
2024-06-26 13:01:01 -0300

    includes mamou when 0% chance of pago

--- tapago.js
+++ tapago.js
@@ -13,7 +13,6 @@ button.addEventListener("click", () => {
 function taPago(inFront, goal) {
   function isPaid() {
     let faltam = calcTimeLeft(goal);
-    console.log(faltam);
     let sum = Math.floor(Math.random() * (25 - 5) + 5);
     for (let x = 0; x < inFront; x++) {
       let mediaLigacao = Math.floor(Math.random() * (25 - 5) + 5);
@@ -26,7 +25,10 @@ function taPago(inFront, goal) {
   for (let i = 0; i < 1000; i++) {
     isPaid() ? pagoCount++ : naoPagoCount++;
   }
-  console.log("pago count" + pagoCount / 200);
-  let result = ((pagoCount / 1000) * 100).toFixed(2) + "%";
-  return "Você tem " + result + " de chance de já estar pago.";
+  console.log("pago count" + pagoCount);
+  let result = (pagoCount / 1000) * 100;
+  console.log(result);
+  return result > 0
+    ? `Você tem ${result.toFixed(1)}% de chance de já estar pago`
+    : "Mamou";
 }
